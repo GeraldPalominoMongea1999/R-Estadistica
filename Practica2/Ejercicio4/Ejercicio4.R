1885e045a62d61cc323dd58cbb11020fdfbf33ca
GeraldPalominoMongea1999 <asecino1999@gmail.com>
2017-11-24 12:18:28 -0500

    modificado:    Practica2/Ejercicio4/Ejercicio4.R

--- Practica2/Ejercicio4/Ejercicio4.R
+++ Practica2/Ejercicio4/Ejercicio4.R
@@ -19,3 +19,29 @@ ultimo <-function(x){
   return(x[length(x)]) 
 }
 ultimo(1:9)
+#b version simple 
+# retornar los valores k esimos esde el final y el ultimo
+primero <-function(x,k){
+  if ((k>length(x))){
+    reptidos<-(k-k%%length(x))/length(x)
+    
+  }
+  return(x[1:k]) 
+}
+primero <-function(x){
+  
+  return(x[1]) 
+}
+ultimo <-function(x,k){
+  if (k>length(x))return(0)
+  return(x[k:length(x)]) 
+}
+ultimo <-function(x){
+  
+  return(x[length(x)]) 
+}
+ultimo(1:9)
+
+
+
+
